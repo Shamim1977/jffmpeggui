180cd448fc51043999a4ec93d7362fa5073a3c00
GHOUST <GHOUST@ca2bccc9-e951-0410-b9b1-f3094c2aa05f>
2008-07-13 16:12:20 +0000

    Initial Commit jffmpegGUI

---
+++ trunk/src/Run.java
@@ -0,0 +1,15 @@
+import gui.MainFrame;
+
+
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainFrame();
+
+	}
+
+}
